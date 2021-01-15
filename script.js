7474034a8c1be00ae8d91dbd5754b502b47daafb
Pascoal Daniel Fernandes <62003990+allopopot@users.noreply.github.com>
2021-01-15 19:12:50 +0530

    Website New Look

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-var panes = ['paneOne', 'paneTwo', 'paneThree', 'paneFour', 'paneFive', 'paneSix', 'paneSeven', 'paneEight'];
-var a = 0;
-document.querySelector('#' + panes[a]).scrollIntoView();
-function scroller(direction) {
+var collapseMenu = document.querySelector("#collapse-menu");
+var collapseButton = document.querySelector("#collapse-button");
 
-    if (direction === 'down') {
-        if (a >= panes.length - 1) return;
-        a = a + 1;
-        document.querySelector('#' + panes[a]).scrollIntoView();
-    } else if (direction === 'up') {
-        if (a == 0) return;
-        a = a - 1;
-        document.querySelector('#' + panes[a]).scrollIntoView();
-    }
-}
-
-
-
-function animatePaneTwo() {
-    document.querySelector('#paneTwo').scrollIntoView();
-    var intro = "Hello, My Name is Pascoal Daniel Fernandes.";
-    var introElement = document.querySelector('#introElement');
-    var i = 0;
-
-    setInterval(function () {
-        if (i < intro.length) {
-            
-            introElement.innerHTML += intro.charAt(i);
-            i++;
-        } else {
-            clearInterval();
-        }
-    }, 100);
-}
+collapseButton.addEventListener('click',()=>{
+    collapseMenu.classList.toggle('hidden');
+    collapseMenu.classList.toggle('flex');
+})
