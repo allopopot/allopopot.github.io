5f6dc342adca78a69a38334db66a0034507f024f
Pascoal Daniel Fernandes <62003990+allopopot@users.noreply.github.com>
2024-02-09 23:13:24 +0400

    updates

---
+++ src/router.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Main from "./pages/Main.vue"
+const routes = [
+    { path: '/', component: Main },
+    // { path: '/resume', component: About },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+export default router
