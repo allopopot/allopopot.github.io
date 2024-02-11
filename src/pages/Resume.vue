b0ae8838968decc2012c3f4ca7a6fbd997aecb2c
Pascoal Daniel Fernandes <62003990+allopopot@users.noreply.github.com>
2024-02-11 17:21:01 +0400

    Resume Page 2 Completed

--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="a4page mx-auto flex border-2">
+    <div class="a4page mx-auto flex">
         <div class="w-3/5 h-full p-4 font-wrap">
             <h1 class="text-4xl underline font-bold mt-5 mb-3">{{ information.firstName }} {{ information.middleName }}
                 {{ information.lastName }}</h1>
@@ -14,7 +14,7 @@
 
             <div>
                 <h2 class="text-2xl font-bold">Education</h2>
-                <ul class="list-disc ml-4">
+                <ul class="list-disc ml-4 list-outside">
                     <li v-for="entry in education" class="my-3">
                         <h3 class="text-xl font-semibold">{{ entry.degree }}</h3>
                         <p class="underline font-semibold "> {{ entry.institution }}</p>
@@ -29,10 +29,10 @@
             <p class="mb-1 text-center font-semibold">{{ information.email }}</p>
             <p class="mb-1 text-center font-semibold"> {{ information.phone.join() }}</p>
             <p class="mb-1 text-center font-semibold">{{ information.currentAddress }}</p>
-
+            
             <h1 class="text-xl mt-4 font-semibold">Key Skill</h1>
             <hr class="border-2 border-white rounded-sm my-2">
-            <ul class="list-disc ml-4">
+            <ul class="list-disc ml-4 list-outside">
                 <li v-for="entry in technologies" class="my-0 font-semibold text-base">{{ entry }}</li>
             </ul>
 
@@ -61,10 +61,31 @@
                 <div>{{ information.languages.join(", ") }}</div>
             </div>
         </div>
+    </div>
 
+    <div class="a4page mx-auto p-6">
+        <hr class="border-2 border-gray-300 rounded-sm my-4">
+        <div>
+            <h2 class="text-2xl font-bold">Work Experience</h2>
+            <ul class="list-disc list-outside ml-4">
+                <li v-for="entry in workExperience.toReversed()" class="my-4">
+                    <h3 class="text-xl font-semibold">{{ entry.designation }} ({{ entry.level }})</h3>
+                    <h4 class="text-lg italic underline font-semibold">{{ entry.companyName }}</h4>
+                    <p class="font-semibold"> {{ entry.start }} - {{ entry.end }} ({{ entry.duration }})</p>
+                    <ul class="list-disc ml-4 mt-1">
+                        <li v-for="subentry in entry.jobDescription">{{ subentry }}</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+        <hr class="border-2 border-gray-300 rounded-sm my-4">
+        <div>
+            <h2 class="text-2xl font-bold">Achievements</h2>
+            <p class="my-3" v-for="entry of achievements">{{ entry }}</p>
+        </div>
     </div>
 </template>
 
 <script setup>
-import { information, education, technologies, summary } from "../assets/data.json"
+import { information, education, technologies, summary, workExperience, achievements } from "../assets/data.json"
 </script>
