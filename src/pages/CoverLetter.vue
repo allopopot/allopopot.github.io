ffa93e0c2607606b22579b8644e98571b07c822b
Pascoal Daniel Fernandes <allopopot@outlook.com>
2024-04-12 20:17:19 +0400

    data changes

--- src/pages/CoverLetter.vue
+++ src/pages/CoverLetter.vue
@@ -14,14 +14,14 @@
             <p>
                 I'm writing to express interest in the {{ positionTitle }} at {{ companyName }}. With expertise in
                 Node.js,
-                MongoDB, Vue.js, and React, I'm transitioning to ASP.NET and C# while nurturing a keen interest in
+                MongoDB, Vue.js, and React, I'm transitioning to Laravel while nurturing a keen interest in
                 DevOps
                 and cloud tech.
             </p>
             <p>
                 My background in web development equips me to adapt quickly to new technologies. I've worked extensively
                 with
-                Node.js, MongoDB, Vue.js, and React, and now seek to leverage my skills in ASP.NET and C#.
+                Node.js, MongoDB, Vue.js, and React, and now seek to leverage my skills in Laravel.
             </p>
             <p>
                 Furthermore, my curiosity about DevOps and cloud technologies like Docker, Podman, Jenkins, Google
